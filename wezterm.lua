f2f70a686d398c454b934d68c5712a7c92118454
Kai Moschcau <kai.moschcau@blecon.de>
2025-10-06 14:52:56 +0200

    feat: add progress display to tabs

--- wezterm.lua
+++ wezterm.lua
@@ -3,6 +3,7 @@ local wezterm = require "wezterm"
 
 local compat = require "system-compat"
 
+---@return "Dark"|"DarkHighContrast"|"Light"|"LightHighContrast"
 local function get_appearance()
   if wezterm.gui then
     return wezterm.gui.get_appearance()
@@ -10,6 +11,8 @@ local function get_appearance()
   return "Dark"
 end
 
+---@param appearance "Dark"|"DarkHighContrast"|"Light"|"LightHighContrast"
+---@return string
 local function scheme_for_appearance(appearance)
   if appearance:find "Dark" then
     return "Catppuccin Mocha"
@@ -17,6 +20,73 @@ local function scheme_for_appearance(appearance)
     return "Catppuccin Latte"
   end
 end
+
+---@param tab_info TabInformation
+---@return string
+local function tab_title(tab_info)
+  local title = tab_info.tab_title
+  -- if the tab title is explicitly set, take that
+  if title and #title > 0 then
+    return title
+  end
+
+  -- Otherwise, use the title from the active pane in that tab
+  return tab_info.active_pane.title
+end
+
+local PCT_GLYPHS = {
+  wezterm.nerdfonts.md_checkbox_blank_circle_outline,
+  wezterm.nerdfonts.md_circle_slice_1,
+  wezterm.nerdfonts.md_circle_slice_2,
+  wezterm.nerdfonts.md_circle_slice_3,
+  wezterm.nerdfonts.md_circle_slice_4,
+  wezterm.nerdfonts.md_circle_slice_5,
+  wezterm.nerdfonts.md_circle_slice_6,
+  wezterm.nerdfonts.md_circle_slice_7,
+  wezterm.nerdfonts.md_circle_slice_8,
+}
+
+---@param pct number
+---@return string
+local function pct_glyph(pct)
+  local slot = math.floor(pct / 12.5)
+  return PCT_GLYPHS[slot + 1]
+end
+
+wezterm.on("format-tab-title", function(tab)
+  ---@type "None"|"Indeterminate"|{ Percentage: number?, Error: number? }
+  local progress = tab.active_pane.progress or "None"
+  local title = tab_title(tab)
+  local elements = {
+    { Text = string.format("%d: ", tab.tab_index + 1) },
+  }
+
+  if progress ~= "None" then
+    local color = "green"
+    local status
+    if progress.Percentage ~= nil then
+      -- status = string.format("%d%%", progress.Percentage)
+      status = pct_glyph(progress.Percentage)
+    elseif progress.Error ~= nil then
+      -- status = string.format("%d%%", progress.Error)
+      status = pct_glyph(progress.Error)
+      color = "red"
+    elseif progress == "Indeterminate" then
+      status = wezterm.nerdfonts.md_dots_circle
+    else
+      ---@diagnostic disable-next-line: param-type-mismatch
+      status = wezterm.serde.json_encode(progress)
+    end
+
+    table.insert(elements, { Foreground = { Color = color } })
+    table.insert(elements, { Text = status .. "  " })
+    table.insert(elements, { Foreground = "Default" })
+  end
+
+  table.insert(elements, { Text = title })
+
+  return elements
+end)
 
 ---@type Config
 local config = {}
